ec53172d8f327f68ab06eb90e87ef0b2429c6531
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2022-12-12 13:23:40 -0600

    async layout

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,12 +2,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import LogoutButton from './LogoutButton';
-import { unstable_getServerSession } from "next-auth/next"
+import { unstable_getServerSession } from "next-auth/next";
 
-async function Header() {
-    const session = await unstable_getServerSession();
+type Props = {
+    session: Awaited<ReturnType<typeof unstable_getServerSession>>;
+}
 
-    if(session) return (
+function Header({ session }: Props) {
+    
+
+    if (session) 
+    return (
         <header className='sticky top-0 z-50 bg-white flex justify-between items-center p-10 shadow-sm'>
             <div className='flex space-x-2'>
                 <Image 
@@ -23,7 +28,8 @@ async function Header() {
             </div>
             <LogoutButton />
         </header>
-    )
+    );
+
   return (
     <header className='sticky top-0 z-50 bg-white flex justify-center items-center p-10 shadow-sm'>
         <div className='flex flex-col items-center space-y-5'>
@@ -40,4 +46,5 @@ async function Header() {
     </header>
   )
 }
+
 export default Header
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,18 @@
 import '../styles/globals.css'
 import Header from './Header';
-//import { unstable_getServerSession } from "next-auth/next";
+import { unstable_getServerSession } from "next-auth/next";
 
 export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-   // const session = await unstable_getServerSession();
+    const session = await unstable_getServerSession();
     return (
         <html>
             <head/>
             <body>
-            {/* <Header session={session}/> */}
-           {/* @ts-ignore */}
-           <Header />
+            <Header session={session}/>
                 {children}
                 </body>
         </html>
